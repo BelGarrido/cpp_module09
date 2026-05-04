d0e07f862082856d4a87013b2d09505d2cbbc645
Ana Garrido Diaz <anaigdx@gmail.com>
2026-05-04 16:40:00 +0200

    add ex00 parser

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <fstream>
 #include <sstream> 
+#include <map>
 
 // pseudocode
 
@@ -31,12 +32,77 @@
 // - empty value
 // - missing |
 
+bool parseYear(std::string &date) {
+    std::string yearStr = date.substr(0, 4);
+    std::cout << "YEAR -------" << yearStr << std::endl;
+    int year;
+}
+
+bool validLine(std::string &line) {
+
+    if(line.size() < 14) {
+        std::cout << "INVALID FORMAT: invalid length" << std::endl;
+        return false;
+    }
+
+    if(line[4] != '-' && line[7] != '-') {
+        std::cout << "INVALID FORMAT: missing '-'" << std::endl;
+        return false;
+    }
+
+    if(line.find('|') != 11) {
+        std::cout << "INVALID FORMAT: missing '|'" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool validDate(std::string &date) {
+    
+    if(date.size() != 11) {
+        std::cout << "INVALID FORMAT: date length" << std::endl;
+        return false;
+    }
+    
+    std::string monthStr = date.substr(5, 2);
+    std::cout << "MONTH -------" << monthStr << std::endl;
+    int month;
+    std::string dayStr = date.substr(8, 2);
+    std::cout << "DAY ---------" << dayStr << std::endl;
+    int day;
+    
+    std::stringstream tmp(yearStr);
+    tmp >> year;
+    if (year < 2009 || year > 2100) {
+        std::cout << "INVALID DATE: year not valid" << std::endl;
+        return false;
+    }
+
+    std::stringstream tmp2(monthStr);
+    tmp2 >> month;
+    if (month < 1 || month > 12) {
+        std::cout << "INVALID DATE: month not valid" << std::endl;
+        return false;
+    }
+    std::stringstream tmp3(dayStr);
+    tmp3 >> day;
+    if (day < 1 || day > 31) {
+        std::cout << "INVALID DATE: day not valid" << std::endl;
+        return false;
+    }
+
+    //luego duvidir en substring y comprobar el valor numerico
+    
+    return true;
+}
+
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 1;
     }
 
-    std::ifstream data("prueba.csv");
+    std::ifstream data("../prueba.csv");
     if(!data.is_open()) {
         std::cout << "ERROR: file could not be opened"  << std::endl; 
         return 1;
@@ -49,16 +115,61 @@ int main(int argc, char *argv[]) {
         std::cout << "ERROR: file is empty, this is no super accurate i need to think about how to improve it"  << std::endl; 
         return 1;
     }
+
+    //empty map initialise
+    std::map<std::string, float> dataHistory;
+
     while (getline(data, line)) {
         std::stringstream ss(line);
         std::string date;
-        std::string value;
+        std::string strValue;
         //std::cout << "before getline" << std::endl;
         getline(ss, date, ',');   // reads until it hits ','
-        getline(ss, value);       // reads the rest of the line
-        std::cout << "date: " << date << " value: " << value << std::endl;
+        getline(ss, strValue);       // reads the rest of the line
+        
+        //transform string to float (not sure is i have to ste the precision?)
+        float value = std::stof(strValue);
+
+        std::cout << "date: " << date << " value: " << strValue << "float: " << value << std::endl;
+        dataHistory.insert({date, value});
     }
-    std::cout << "here" << std::endl;    
+    for (std::map<std::string, float>::iterator record=dataHistory.begin(); record != dataHistory.end(); ++record) {
+            std::cout << record->first << " => " << record->second << std::endl;
+    }
+    std::cout << "here" << std::endl;
+
+    std::ifstream input("../input.txt");
+    if(!input.is_open()) {
+        std::cout << "ERROR: file could not be opened"  << std::endl; 
+        return 1;
+    }
+    getline(input, line);
+    while (getline(input, line)) {
+        if(!validLine(line)) {
+            std::cout << "INVALID" << std::endl;
+            return 1;
+        }
+        std::stringstream ss(line);
+        std::string date;
+        std::string strValue;
+        std::cout << "INPUT" << std::endl;
+        getline(ss, date, '|');   // reads until it hits ','
+        getline(ss, strValue);       // reads the rest of the line
+        
+        if(!validDate(date)) {
+            std::cout << "valid date" << std::endl;
+        }
+        //if(!validValue(strValue)) {
+
+        //}
+        //transform string to float (not sure is i have to ste the precision?)
+        //float value = std::stof(strValue);
+        //validate format
+        
+        //compare
+    }
+    std::cout << "Hasta aqui bien" << std::endl;
+    return 0;
 }
 
 
